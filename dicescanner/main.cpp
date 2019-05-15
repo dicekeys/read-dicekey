503aee6d15be664529f1fa04710cb2f4a33fb1b6
Stuart Schechter <stuart@Stuarts-MacBook-Pro.local>
2019-05-15 16:34:41 +0900

    get running on mac (but breaking windows)

---
+++ dicescanner/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  dicesacanner
+//
+//  Created by Stuart Schechter on 15/05/2019.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
