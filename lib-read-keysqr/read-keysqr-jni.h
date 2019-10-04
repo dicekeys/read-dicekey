427ded5020f8cd73177d77ed8cb1dfbe2d260e97
Stuart Schechter <stuart@Stuarts-MacBook-Pro.local>
2019-10-04 15:15:11 +0900

    sketch out jni a bit more

--- lib-read-keysqr/read-keysqr-jni.h
+++ lib-read-keysqr/read-keysqr-jni.h
@@ -9,8 +9,36 @@
 //     jint bytesPerRow,
 //     jbyteArray jByteArrayForGrayscaleChannel)
 // {
-//     jbyte* pointerToByteArrayForGrayscaleChannel= env->GetByteArrayElements(jByteArrayForGrayscaleChannel, 0);
-//     const std::string jsonResult = readKeySqrJson(width, height, pointerToByteArrayForGrayscaleChannel, bytesPerRow)
-//     env->ReleaseByteArrayElements(p_data, pointerToByteArrayForGrayscaleChannel, 0);
-//     return jsonResult;
+// 	const std::string jsonResult;
+// 	jbyte* pointerToByteArrayForGrayscaleChannel = env->GetByteArrayElements(jByteArrayForGrayscaleChannel, 0);
+// 	if (pointerToByteArrayForGrayscaleChannel != null) {
+// 		try {
+// 			jsonResult = readKeySqrJson(width, height, pointerToByteArrayForGrayscaleChannel, bytesPerRow)
+// 		} catch (...) {
+// 			jsonResult = "null";
+// 		}
+// 	}
+// 	env->ReleaseByteArrayElements(p_data, pointerToByteArrayForGrayscaleChannel, 0);
+// 	return jsonResult;
 // }
+
+// read-keysqr
+// base c++ project
+//   dependent on OpenCV being available
+//   should be entirely platform independent
+//   if absolutely necessary, can use ifdefs for ANDROID, IOS, etc.
+
+// keysqr-jvm
+//   exposes android api using JNI
+//     imports c++ directly
+//   importable either by
+//     `git submodule` or
+//     `jitpack`
+
+// keysqr-ios
+//   exposes ios api
+
+// react-native-camera
+//   existing project we are forking
+//   imports keysqr-android for android side, keysqr-ios for ios side
+
