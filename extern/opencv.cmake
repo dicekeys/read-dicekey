6aee778294b6098527db45fa4d247902863ca004
Fred Njira <frednjira005@gmail.com>
2019-11-10 04:34:48 +0800

    Used FetchContent in OpenCV

--- extern/opencv.cmake
+++ extern/opencv.cmake
@@ -1,31 +1,10 @@
-set (Extern_DIR "${CMAKE_SOURCE_DIR}/extern")
+include(FetchContent)
 
-if(CMAKE_SYSTEM_NAME STREQUAL "Android")
-    set (Cross_OpenCV_DIR "${Extern_DIR}/OpenCV-android-sdk/sdk/native/jni")
-else()
-    set (Cross_OpenCV_DIR "${Extern_DIR}/opencv/build")
-endif()
+FetchContent_Declare(
+    opencv
+    GIT_REPOSITORY https://github.com/opencv/opencv.git
+)
+set(BUILD_LIST "core,imgproc,imgcodecs" CACHE STRING "")
+FetchContent_MakeAvailable(opencv)
 
-set (OpenCV_DIR ${Cross_OpenCV_DIR})
-find_package(OpenCV COMPONENTS core imgproc imgcodecs)
-
-if(NOT OpenCV_core_FOUND)
-
-    if(CMAKE_SYSTEM_NAME STREQUAL "Android")
-
-        file(DOWNLOAD "https://github.com/opencv/opencv/releases/download/4.1.2/opencv-4.1.2-android-sdk.zip" "${Extern_DIR}/opencv-android.zip")
-        execute_process(COMMAND "${CMAKE_COMMAND}" -E tar xzf "${Extern_DIR}/opencv-android.zip" WORKING_DIRECTORY "${Extern_DIR}")
-        file(REMOVE "${Extern_DIR}/opencv-android.zip")
-
-    else()
-
-        message("Compiling OpenCV library")
-        execute_process(COMMAND "${CMAKE_COMMAND}" -E make_directory "${Cross_OpenCV_DIR}")
-        execute_process(COMMAND "${CMAKE_COMMAND}" "-DBUILD_LIST=core,imgproc,imgcodecs" ".." WORKING_DIRECTORY "${Cross_OpenCV_DIR}")
-        execute_process(COMMAND "${CMAKE_COMMAND}" "--build" "." WORKING_DIRECTORY "${Cross_OpenCV_DIR}")
-
-    endif()
-
-    set (OpenCV_DIR ${Cross_OpenCV_DIR})
-    find_package(OpenCV REQUIRED COMPONENTS core imgproc imgcodecs)
-endif()
+find_package(OpenCV REQUIRED COMPONENTS core imgproc imgcodecs)
