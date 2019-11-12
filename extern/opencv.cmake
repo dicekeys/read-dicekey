77f413309b3bf8dd28061c5d1d17a149e67f63e6
Stuart Schechter <stuart.schechter@gmail.com>
2019-11-12 10:35:17 +0900

    current hack to download opencv

--- extern/opencv.cmake
+++ extern/opencv.cmake
@@ -5,43 +5,38 @@ message("CMAKE_SYSTEM_NAME=${CMAKE_SYSTEM_NAME}")
 if(CMAKE_SYSTEM_NAME STREQUAL "Android")
     FetchContent_Declare(
         opencv
-        URL https://github.com/opencv/opencv/releases/download/4.1.2/opencv-${OPENCV_VERSION}-android-sdk.zip
+        URL https://github.com/opencv/opencv/releases/download/${OPENCV_VERSION}/opencv-${OPENCV_VERSION}-android-sdk.zip
+        UPDATE_DISCONNECTED # since we're using a fixed version, only download once
     )
 	FetchContent_MakeAvailable(opencv)
     set (OpenCV_DIR "${opencv_SOURCE_DIR}/sdk/native/jni")
 	find_package(OpenCV ${OPENCV_VERSION} REQUIRED COMPONENTS core imgproc imgcodecs)
 
-else()
-#    message("Candidate SOURCE_DIR: ${CMAKE_CURRENT_SOURCE_DIR}/extern/opencv")
-#    message("Candidate BINARY_DIR: ${CMAKE_CURRENT_BINARY_DIR}/opencv")
+elsif(CMAKE_SYSTEM_NAME STREQUAL "Windows")
+    message("Downloading OpenCV for windows(this may take many minutes and possibly timeout!)")
     FetchContent_Declare(
         opencv
-        URL https://github.com/opencv/opencv/archive/${OPENCV_VERSION}.zip
+#       We had previously downloaded the source git, but
+#       that doesn't have working headers because OpenCV
+#       sucks 
 #        GIT_REPOSITORY https://github.com/opencv/opencv.git
-#        SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/extern/opencv
-#        BINARY_DIR: ${CMAKE_CURRENT_BINARY_DIR}/opencv
+        URL https://github.com/opencv/opencv/releases/download/${OPENCV_VERSION}/opencv-${OPENCV_VERSION}-vc14_vc15.exe
+        UPDATE_DISCONNECTED # since we're using a fixed version, only download once
     )
-    set(BUILD_LIST "core,imgproc,imgcodecs" CACHE STRING "")
 	FetchContent_MakeAvailable(opencv)
-#    install(opencv)
-#	find_package(OpenCV ${OPENCV_VERSION} REQUIRED COMPONENTS core imgproc imgcodecs)
+    message("Finished downloading opencv")
+    set (OpenCV_DIR "${opencv_SOURCE_DIR}/build")
+    message("OpenCV_DIR ${OpenCV_DIR}")
 
-    # set(OpenCV_LIBS "opencv_core;opencv-imgproc;opencv-imgcodecs")
-	#    set (OpenCV_DIR "${CMAKE_BINARY_DIR}")
+	find_package(OpenCV 4.1.2 REQUIRED COMPONENTS core imgproc imgcodecs)
+
 	message("OpenCV_DIR = ${OpenCV_DIR}")
 	message("OpenCV_SOURCE_DIR = ${OpenCV_SOURCE_DIR}")
 	message("OpenCV_BINARY_DIR = ${OpenCV_BINARY_DIR}")
 	message("OpenCV_INCLUDE_DIR = ${OpenCV_INCLUDE_DIR}")
 	message("OpenCV_LIBS = ${OpenCV_LIBS}")
-
     add_library(opencv SHARED IMPORTED)
-    # set_target_properties(opencv PROPERTIES
-    #  INTERFACE_INCLUDE_DIRECTORIES "${OpenCV_INCLUDE_DIRS}"
-    #  IMPORTED_LOCATION "${OpenCV_LIBS}")
 
+else()
+    ## FIXME -- need a solution that works on mac & linux
 endif()
-
-# add_library(opencv SHARED IMPORTED)
-#set_target_properties(opencv PROPERTIES
-#  INTERFACE_INCLUDE_DIRECTORIES "${OpenCV_INCLUDE_DIRS}"
-#  IMPORTED_LOCATION "${OpenCV_LIBS}")
